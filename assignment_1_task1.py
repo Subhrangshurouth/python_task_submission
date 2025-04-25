0b52769076111fdb72db71e01dabad2abec26d2f
Subhrangshu Routh <118276361+Subhrangshurouth@users.noreply.github.com>
2025-04-25 09:45:25 +0530

    Add files via upload

---
+++ assignment_1_task1.py
@@ -0,0 +1,16 @@
+#taking input from user
+a=input("enter the first number:")
+b=input("Enter the second number:")
+#assign the integer data type to the variable
+a=int(a)
+b=int(b)
+#Apply all the operations
+s=a+b
+c=a-b
+d=a*b
+e=a/b
+#print the desired output
+print("Addition:",s)
+print("Subtraction:",c)
+print("Multiplication:",d)
+print("Division:",e)
---
+++ assignment_1_task_2.py
@@ -0,0 +1,9 @@
+# take input from user
+a=input("Enter your first name:")
+b=input("Enter your last name:")
+#assign data type
+a=str(a)
+b=str(b)
+# print the output
+print("Hello,",a,b,"! Welcome to the python programme.")
+
