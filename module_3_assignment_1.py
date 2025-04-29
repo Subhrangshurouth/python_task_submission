c67553294c26bd53ba5e12635fbd53193044adfe
Subhrangshu Routh <118276361+Subhrangshurouth@users.noreply.github.com>
2025-04-29 12:54:24 +0530

    Add files via upload

---
+++ module_3_assignment_1.py
@@ -0,0 +1,7 @@
+#taking input from user
+number=int(input("Enter a number:"))
+if (number%2==0):
+    print(number,"is an even number")
+else:
+    print(number,"is an odd number")
+
---
+++ module_3_assignment_2.py
@@ -0,0 +1,6 @@
+#initial sum value is 0
+sum=0
+#apply for loop with range function 0 to 51
+for i in range(0,51):
+    sum=sum+i
+print("The sum of numbers from 1 to 50 is:",sum)
